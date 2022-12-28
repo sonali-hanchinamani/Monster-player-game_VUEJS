eda407114c05dc8e5f3f5c7d2622ce695115d9db
Sonali c h <51365252+sonali-hanchinamani@users.noreply.github.com>
2022-12-28 23:42:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+function generateRandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+const app = Vue.createApp({
+    data(){
+        return{
+            playerHealth:100,
+            monsterHealth:100,
+            currentRound:0,
+            winner: null,
+            logMessages:[]
+        }
+    },
+    computed:{
+        monsterBarStyles(){
+            if(this.monsterHealth <0){
+                return { width: '0%'};
+            }
+            return {width: this.monsterHealth+ '%'}
+        },
+        playerBarStyles(){
+            if(this.playerHealth <0){
+                return { width: '0%'};
+            }
+            return {width: this.playerHealth+ '%'}
+        },
+        specialAttack(){
+            return  this.currentRound % 3 !== 0
+        }
+    },
+    watch:{
+        playerHealth(value){
+            if(value <=0 && this.playerHealth<= 0){
+                this.winner ='draw'
+            }else if(value<=0){
+                this.winner = 'player';
+            }
+        },
+        monsterHealth(value){
+            if(value <=0 && this.monsterHealth<= 0){
+                this.winner ='draw';
+            }else if(value<=0){
+                this.winner = 'monster';
+            }
+        }
+    },
+    methods:{
+        startGame(){
+            this.playerHealth=100,
+            this.monsterHealth=100,
+            this.currentRound=0,
+            this.winner= null,
+            this.logMessage=[]
+        },
+        monsterAttack(){
+            this.currentRound++
+            const attackValue = generateRandom(5,12)
+            this.monsterHealth -=attackValue
+            this.addLogMessage('player','attack',attackValue)
+            this.playerAttack()
+        },
+        playerAttack(){
+            const attackValue = generateRandom(8,15)
+            this.playerHealth -=attackValue
+            this.addLogMessage('monster','attack',attackValue)
+
+        },
+        specialAttackMonster(){
+            const attackValue= generateRandom(10,25)
+            this.monsterHealth -=attackValue;
+            this.addLogMessage('player','attack',attackValue)
+            this.playerAttack();
+        },
+        healPlayer(){
+            this.currentRound++
+            const healValue = generateRandom(8,20)
+            if(this.playerHealth + healValue> 100)
+                this.playerHealth= 100
+            else this.playerHealth += healValue 
+            this.addLogMessage('player', 'heal', healValue);
+            this.playerAttack()
+        },
+        surrender(){
+            this.winner= 'monster'
+        },
+        addLogMessage(who, what, value) {
+            this.logMessages.unshift({
+              actionBy: who,
+              actionType: what,
+              actionValue: value
+            });
+          }
+        
+    }
+})
+
+app.mount('#game')
